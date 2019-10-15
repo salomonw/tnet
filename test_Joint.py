40e7aa29c4ec4453a8d885613a22dbe46437ad81
salomonw <salomonw@bu.edu>
2019-10-15 13:56:48 -0400

    add yey ney

--- test_Joint.py
+++ test_Joint.py
@@ -12,8 +12,8 @@ from datetime import datetime
  
 
 def set_up():
-	netFile ="networks/Braess2_net.txt"
-	gFile = "networks/Braess2_trips.txt"
+	netFile ="networks/NYC_net.txt"
+	gFile = "networks/NYC_trips.txt"
 	# Build a ground truth network
 	fcoeffs_truth = [1,0,0,0,0.35,0]
 	tNet = tnet.tNet(netFile=netFile, gFile=gFile, fcoeffs=fcoeffs_truth)
@@ -22,8 +22,8 @@ def set_up():
 	G_data = tNet.G.copy()
 	g_data = tNet.g.copy()
 
-	c1 = 150
-	d1 = 0.02
+	c1 = 100
+	d1 = 0.005
 
 	# Now, use the data to try to estimate demands, to do so 
 	# let us perturb the truth demands. 
@@ -31,10 +31,13 @@ def set_up():
 	fcoeff = [1,0,0,0,0.15,0]
 
 	to_solve = ["constant", "GD", "alternating", "Joint"]
-	iterations = 60
+	iterations = 10
 
 	tstamp = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
 	dir_out = tstamp + "_test_" + tNet.netFileName[9:-8]
+
+	generate_plots = True
+
 	# Create directories to store results
 	mkdir_n('results/')
 	mkdir_n('results/joint')
@@ -44,7 +47,7 @@ def set_up():
 	mkdir_n('results/joint/' + dir_out +'/output')
 	dir_out = 'results/joint/' + dir_out 
 
-	return [G_data, g_data, g_k, fcoeff, tNet, c1, d1, fcoeffs_truth, to_solve, iterations, dir_out]
+	return [G_data, g_data, g_k, fcoeff, tNet, c1, d1, fcoeffs_truth, to_solve, iterations, dir_out, generate_plots]
 
 
 
@@ -114,14 +117,11 @@ def solve_od_fcoffs(G_data, g_data, g_k, fcoeff, tNet, opt_method, iterations, c
 	return tNet, flowNormList, gNormlist, fcoeff
 
 
-
-
-
 # Run experiments for different algorithms
 
 fcoeffs_list = []
 parameters = set_up()
-[G_data, g_data, g_0, fcoeffs_0, tNet, c1, d1, fcoeffs_truth, to_solve, iterations, dir_out] = parameters
+[G_data, g_data, g_0, fcoeffs_0, tNet, c1, d1, fcoeffs_truth, to_solve, iterations, dir_out, generate_plots] = parameters
 x_axis  = [i+1 for i in range(iterations-1)]
 tNet_0 = copy.deepcopy(tNet)
 if "constant" in to_solve:
@@ -153,6 +153,7 @@ if "Joint" in to_solve:
 
 
 # Save results to files
+
 list2txt(flowNormConstant, dir_out + "/iterations/"+tNet.netFileName[9:-8]+'_flowNormConstant.txt')
 list2txt(flowNormGD, dir_out +"/iterations/"+tNet.netFileName[9:-8]+'_flowNormGD.txt')
 list2txt(flowNormAlternating, dir_out +"/iterations/"+tNet.netFileName[9:-8]+'_flowNormAlternating.txt')
@@ -164,55 +165,62 @@ list2txt(gNormJOINT, dir_out +"/iterations/"+tNet.netFileName[9:-8]+'_flowNormJO
 list2txt(parameters, dir_out +"/"+tNet.netFileName[9:-8]+'_parameters.txt')
 
 
+# Identify if the demand estimation is 'good enough'
+if np.mean(flowNormJOINT)/np.mean(flowNormConstant) - 1 < .1:
+	list2txt(parameters, dir_out +"/"+tNet.netFileName[9:-8]+'YEY')
+else:
+	list2txt(parameters, dir_out +"/"+tNet.netFileName[9:-8]+'NEY')
+
+
 # Generate Plots
+if generate_plots:
+	mpl.rc('font',**{'family':'Times New Roman', 'size': 16})
+	mpl.rc('text', usetex=True)
+	mpl.rc('text', usetex=True)
+	mpl.rcParams['text.latex.preamble']=[r"\usepackage{amsmath}"]
+	plt.figure()
+	plt.plot(x_axis, flowNormConstant[1:], label='$f(\\cdot) =$ BPR', marker='s', markevery=(0,20))
+	plt.plot(x_axis, flowNormGD[1:], label='GD', marker = 'o', markevery=(0,10))
+	plt.plot(x_axis, flowNormAlternating[1:], label='Alternating', marker=11, markevery=(0,25))
+	plt.plot(x_axis, flowNormJOINT[1:], label='Joint', marker = 'o', markevery=(0,10))
+	plt.xlabel("Iteration,$j$")
+	plt.ylabel("Flow error, $F(\\mathbf{g}, \\boldsymbol{\\beta})$")
+	plt.legend(framealpha=1)
+	plt.grid(linestyle='--', linewidth=1)
+	plt.savefig(dir_out +'/graphs/'+tNet.netFileName[9:-8]+'_errorCost_single.png')
 
-#mpl.rc('font',**{'family':'Times New Roman', 'size': 16})
-#mpl.rc('text', usetex=True)
-#mpl.rc('text', usetex=True)
-#mpl.rcParams['text.latex.preamble']=[r"\usepackage{amsmath}"]
-#plt.figure()
-#plot(x_axis, flowNormConstant[1:], label='$f(\\cdot) =$ BPR', marker='s', markevery=(0,20))
-#plot(x_axis, flowNormGD[1:], label='GD', marker = 'o', markevery=(0,10))
-#plot(x_axis, flowNormAlternating[1:], label='Alternating', marker=11, markevery=(0,25))
-#plt.plot(x_axis, flowNormJOINT[1:], label='Joint', marker = 'o', markevery=(0,10))
-#xlabel("Iteration,$j$")
-#ylabel("Flow error, $F(\\mathbf{g}, \\boldsymbol{\\beta})$")
-#legend(framealpha=1)
-#grid(linestyle='--', linewidth=1)
-#savefig(dir_out +'/graphs/'+tNet.netFileName[9:-8]+'_errorCost_single.png')
+	plt.figure()
+	plt.plot(x_axis, gNormConstant[1:], label='$f(\\cdot) =$ BPR', marker='s', markevery=(0,20))
+	plt.plot(x_axis, gNormGD[1:], label='GD', marker = 'o', markevery=(0,10))
+	plt.plot(x_axis, gNormAlternating[1:], label='Alternating', marker=11, markevery=(0,25))
+	plt.plot(x_axis, gNormJOINT[1:], label='Joint', marker = 'o', markevery=(0,10))
+	plt.xlabel("Iteration,$j$")
+	plt.ylabel("Demand error, $||(\\mathbf{g} - \\mathbf{g}^{*})||$")
+	plt.legend(framealpha=1)
+	plt.grid(linestyle='--', linewidth=1)
+	plt.savefig(dir_out +'/graphs/'+tNet.netFileName[9:-8]+'_errorDemand_single.png')
 
-#plt.figure()
-#plt.plot(x_axis, gNormConstant[1:], label='$f(\\cdot) =$ BPR', marker='s', markevery=(0,20))
-#plt.plot(x_axis, gNormGD[1:], label='GD', marker = 'o', markevery=(0,10))
-#plt.plot(x_axis, gNormAlternating[1:], label='Alternating', marker=11, markevery=(0,25))
-#plt.plot(x_axis, gNormJOINT[1:], label='Joint', marker = 'o', markevery=(0,10))
-#plt.xlabel("Iteration,$j$")
-#plt.ylabel("Demand error, $||(\\mathbf{g} - \\mathbf{g}^{*})||$")
-#plt.legend(framealpha=1)
-#plt.grid(linestyle='--', linewidth=1)
-#plt.savefig(dir_out +'/graphs/'+tNet.netFileName[9:-8]+'_errorDemand_single.png')
+	def f_cost(x, fcoe):
+		return sum([fcoe[i]*x**i for i in range(len(fcoe))])
 
-#def f_cost(x, fcoe):
-#	return sum([fcoe[i]*x**i for i in range(len(fcoe))])
+	labels = to_solve.copy()
+	labels.append('ground truth')
+	labels[labels.index('constant')] = '$f(\\cdot) =$ BPR'
+	markers = ['o', 'D', 's', '^', '*']
+	fcoeffs_list.append(fcoeffs_truth)
+	plt.figure()
 
-#labels = to_solve.copy()
-#labels.append('ground truth')
-#labels[labels.index('constant')] = '$f(\\cdot) =$ BPR'
-#markers = ['o', 'D', 's', '^', '*']
-#fcoeffs_list.append(fcoeffs_truth)
-#plt.figure()
-
-#x =  np.linspace(0, 1.5, 10, endpoint=True)
-#i=0
-#for i in range(len(labels)):
-#	fcoeffs =fcoeffs_list[i]
-#	y = [f_cost(float(j),fcoeffs) for j in x]
-#	plt.plot(x, y, label=labels[i], marker=markers[i])
-#	i+=1
-#plt.xlabel("$x/m$")
-#plt.ylabel("Travel time function, $f(x/m)$")
-#plt.legend(framealpha=1)
-#plt.grid(linestyle='--', linewidth=1)
-#plt.savefig(dir_out +'/graphs/'+tNet.netFileName[9:-8]+'_costFunct.png')
+	x =  np.linspace(0, 1.5, 10, endpoint=True)
+	i=0
+	for i in range(len(labels)):
+		fcoeffs =fcoeffs_list[i]
+		y = [f_cost(float(j),fcoeffs) for j in x]
+		plt.plot(x, y, label=labels[i], marker=markers[i])
+		i+=1
+	plt.xlabel("$x/m$")
+	plt.ylabel("Travel time function, $f(x/m)$")
+	plt.legend(framealpha=1)
+	plt.grid(linestyle='--', linewidth=1)
+	plt.savefig(dir_out +'/graphs/'+tNet.netFileName[9:-8]+'_costFunct.png')
 
 
